f9796f555fdc8a5409fc387d9815a39446da3850
Joe W <whobbes22@gmail.com>
2023-05-12 10:55:37 -0700

    WIP UI integration with controllers

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -9,9 +9,14 @@ namespace OrderTracker.Models
     Properties to include:
     title, the description, the price, the date, other properties
     */
-    public Order()
+    public string Description {get; set;}
+    public string Title {get; set;}
+    public string Price {get; set;}
+    public Order(string title,string price,string description)
     {
-
+      Description = description;
+      Price = price;
+      Title = title;
     }
 
   }
