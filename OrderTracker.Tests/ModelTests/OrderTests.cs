8388e61a2300f2a7a9bbf4160202874d69d3b685
Joe W <whobbes22@gmail.com>
2023-05-12 09:50:43 -0700

    passed 7th test

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -11,7 +11,7 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order cake = new Order("title","a");
+      Order cake = new Order("title","1","a");
       Assert.AreEqual(typeof(Order), cake.GetType());
     }
 
@@ -19,7 +19,7 @@ namespace OrderTracker.Tests
     public void GetDescription_ReturnsDescription_String()
     {
       string description = "a good cake";
-      Order ord = new Order("1","a",description);
+      Order ord = new Order("a","1",description);
       string result = ord.Description;
 
       Assert.AreEqual(description, result);
@@ -29,7 +29,7 @@ namespace OrderTracker.Tests
     public void GetTitle_ReturnsTitle_String()
     {
       string title = "Cake1";
-      Order ord = new Order("1",title,"description");
+      Order ord = new Order(title,"1","description");
       string result = ord.Title;
 
       Assert.AreEqual(title, result);
@@ -39,7 +39,7 @@ namespace OrderTracker.Tests
     public void GetPrice_ReturnsPrice_String()
     {
       string price = "10.1";
-      Order ord = new Order(price,"title","description");
+      Order ord = new Order("title",price,"description");
       string result = ord.Price;
 
       Assert.AreEqual(price, result);
