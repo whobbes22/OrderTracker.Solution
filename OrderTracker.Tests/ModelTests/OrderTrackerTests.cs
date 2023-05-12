08e6446dcbc65553c17d59b492b343041ae7cb8e
Joe W <whobbes22@gmail.com>
2023-05-12 09:23:46 -0700

    add third test getting name of vendor

--- OrderTracker.Tests/ModelTests/OrderTrackerTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTrackerTests.cs
@@ -11,7 +11,7 @@ namespace ToDoList.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor joe = new Vendor("hi");
+      Vendor joe = new Vendor("joe","hi");
       Assert.AreEqual(typeof(Vendor), joe.GetType());
     }
 
@@ -20,11 +20,22 @@ namespace ToDoList.Tests
     public void GetDescription_ReturnsDescription_String()
     {
       string description = "a nice place";
-      Vendor ven = new Vendor(description);
+      Vendor ven = new Vendor("joe",description);
       string result = ven.Description;
 
       //Assert
       Assert.AreEqual(description, result);
+    }
+    
+    [TestMethod]
+    public void GetName_ReturnName_String()
+    {
+      string name = "joe";
+      Vendor ven = new Vendor(name,"somewhere nice");
+      string result = ven.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
     }
    
   }
