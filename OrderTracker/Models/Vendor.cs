6919a9d8f3418a6440400a61fa2448c57fef67d4
Joe W <whobbes22@gmail.com>
2023-05-12 11:24:18 -0700

    WIP Vendor UI

--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -12,11 +12,17 @@ namespace OrderTracker.Models
 
     public string Name {get;set;}
     public string Description {get; set;}
+    public int Id {get;}
+    private static List<Vendor> _instances = new List<Vendor> {};
     
+    public List<Order> Orders {get;set;}
     public Vendor(string name, string script)
     {
       Description = script;
       Name = name;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
     }
   }
   
